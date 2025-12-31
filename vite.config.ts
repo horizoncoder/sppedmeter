8132ee6eec3242b11a2ba3e94a9e66831fe0f8bd
Dmytro Belymenko <41953956+horizoncoder@users.noreply.github.com>
2025-12-31 11:47:27 +0200

    build

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: './', // Важно для корректной загрузки ассетов на GitHub Pages
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
