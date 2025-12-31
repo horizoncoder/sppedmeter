1d45734869381a5655c32d7525967e59719edaa0
Dima Belimenko <41953956+horizoncoder@users.noreply.github.com>
2025-12-31 09:56:57 +0000

    fix types

--- types.ts
+++ types.ts
@@ -1,21 +1,12 @@
+export interface SpeedometerProps {
+  value: number; // 0 to 100
+  title?: string;
+  subtitle?: string;
+  className?: string;
+}
 
-{
-  "compilerOptions": {
-    "target": "ESNext",
-    "useDefineForClassFields": true,
-    "lib": ["DOM", "DOM.Iterable", "ESNext"],
-    "allowJs": false,
-    "skipLibCheck": true,
-    "esModuleInterop": false,
-    "allowSyntheticDefaultImports": true,
-    "strict": true,
-    "forceConsistentCasingInFileNames": true,
-    "module": "ESNext",
-    "moduleResolution": "Node",
-    "resolveJsonModule": true,
-    "isolatedModules": true,
-    "noEmit": true,
-    "jsx": "react-jsx"
-  },
-  "include": ["./**/*.ts", "./**/*.tsx"]
+export enum ReliabilityLevel {
+  RELIABLE = "Reliable",
+  FUZZY = "Fuzzy",
+  UNRELIABLE = "Unreliable",
 }
